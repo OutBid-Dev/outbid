8e7e8ee2cf102683322f7c3c5c550ab2163ce519
AdityaP183 <adityaprasad1828@gmail.com>
2026-09-21 21:20:46 +0530

    feat(web): initialize frontend application

---
+++ web/vite.config.ts
@@ -0,0 +1,18 @@
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
+})
